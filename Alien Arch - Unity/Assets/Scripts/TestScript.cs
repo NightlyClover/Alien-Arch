52ec52a1b9c7e7fe7a4f2681134b33f123b25fd2
NightlyClover <2038862@fife.ac.uk>
2023-02-27 15:57:47 +0000

    Updated scripts

--- Alien Arch - Unity/Assets/Scripts/TestScript.cs
+++ Alien Arch - Unity/Assets/Scripts/TestScript.cs
@@ -7,10 +7,46 @@ public class TestScript : MonoBehaviour
     public string startUpdate;
     public string startMessage;
 
+    public string message1;
+    public string message2;
+    public string message3;
+    public int messageChoice;
+
+    public string[] messages; 
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.LogWarning(startMessage);
+        //Debug.LogError(startMessage);
+
+        /*
+        if (messageChoice == 1)
+        {
+            Debug.Log(messages[0]);
+        }
+        else if (messageChoice == 2)
+        {
+            Debug.Log(messages[1]);
+        }
+        else if (messageChoice == 3)
+        {
+            Debug.Log(messages[2]);
+        }
+        else
+        {
+            Debug.Log("Invalid message choice");
+        }
+        */
+
+        if (messages.Length > messageChoice && messageChoice >= 0)
+        {
+            Debug.Log(messages[messageChoice]);
+        }
+        else
+        {
+            Debug.Log("Invalid message choice");
+        }
+
     }
 
     // Update is called once per frame
