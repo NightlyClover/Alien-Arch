7bca54ac94523ee3b009bd8d6addd690efa77fa6
NightlyClover <2038862@fife.ac.uk>
2023-03-27 15:32:13 +0100

    Added jump button

--- Alien Arch - Unity/Assets/Scripts/PlayerMovement.cs
+++ Alien Arch - Unity/Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,13 @@ public class PlayerMovement : MonoBehaviour
 {
     //Reference to a RigidBody2D component
     private Rigidbody2D physicsBody = null;
+    //Speed setting m/s
+    public float speed = 1;
+    //Jump speed setting m/s
+    public float jumpSpeed = 10;
+    //Ground sensing stuff
+    public Collider2D groundSensor = null;
+    public LayerMask groundLayer = 0;
 
 
     public void MoveLeft()
@@ -15,7 +22,7 @@ public class PlayerMovement : MonoBehaviour
         //From the physics component
         Vector2 newVelocity = physicsBody.velocity;
         //Set our velocity to move in the negative x (left) direction
-        newVelocity.x = -1;
+        newVelocity.x = -speed;
         //Update our physics component's velocity to be our newly changed value
         physicsBody.velocity = newVelocity;
     }
@@ -26,10 +33,25 @@ public class PlayerMovement : MonoBehaviour
         //From the physics component
         Vector2 newVelocity = physicsBody.velocity;
         //Set our velocity to move in the negative x (left) direction
-        newVelocity.x = 1;
+        newVelocity.x = speed;
         //Update our physics component's velocity to be our newly changed value
         physicsBody.velocity = newVelocity;
     }
+
+    public void Jump()
+    {
+        if (groundSensor.IsTouchingLayers(groundLayer))
+        {
+            //Make a variable to hold the velocity and get the
+            //Current velocity from the physics component
+            Vector2 newVelocity = physicsBody.velocity;
+            //Set our velocity to move in the positive y (up) direction
+            newVelocity.y = jumpSpeed;
+            //Update our physics component velocity to be our newly changed value
+            physicsBody.velocity = newVelocity;
+        }
+    }
+
 
 
     private void Awake()
